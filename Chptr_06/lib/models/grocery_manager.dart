7988b3f9046f0c2b71ecf74ae406320b830371ac
OSX <osx@OSXs-Mac-mini.local>
2022-08-23 17:41:28 +0200

    working on grocery items central state

---
+++ Chptr_06/lib/models/grocery_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'grocery_item.dart';
+
+class GroceryManager extends ChangeNotifier {
+  final _groceryItems = <GroceryItem>[];
+
+  List<GroceryItem> get groceryItems => List.unmodifiable(_groceryItems);
+}
